59a7ec2402e24b91745e886193b20395a56ba93d
PatricioIgnacio <patricioignacio.santibanezgonz@gmail.com>
2023-02-17 13:02:58 -0300

    New changes

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("htmlcss");
-        habilidades[1].classList.add("javascript");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
         habilidades[2].classList.add("reactjs");
         habilidades[3].classList.add("nodejs");
         habilidades[4].classList.add("angular");
